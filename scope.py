fd361496ef80cde78c66ad2a0c3300f8a2e99251
Richard <rchrdgermond@gmail.com>
2017-08-22 12:24:54 -0400

    removed shebangs from files

---
+++ scope.py
@@ -0,0 +1,46 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+class TAXScope:
+    def __init__(self, fs, n):
+        plt.ion()
+        self.fig, self.axarr = plt.subplots(3, sharex=True)
+
+        #setup appearance of figure
+        self.fig.suptitle('CUTE Triaxial Accelerometers', fontsize=16, fontweight='bold')
+        self.axarr[0].set_title('TAXX', fontsize=14)
+        self.axarr[1].set_title('TAXY', fontsize=14)
+        self.axarr[2].set_title('TAXZ', fontsize=14)
+        self.axarr[1].set_ylabel('Acceleration (g)', fontsize=14)
+        self.axarr[2].set_xlabel('Time (s)', fontsize=14)
+
+        #initialize the line objects
+        t = np.linspace(0, n/fs, n)
+        y = np.zeros(n)
+
+        self.line1, = self.axarr[0].plot(t, y, 'b.')
+        self.line2, = self.axarr[1].plot(t, y, 'r.')
+        self.line3, = self.axarr[2].plot(t, y, 'g.')
+
+    def draw(self, y1, y2, y3):
+        self.line1.set_ydata(y1)
+        self.line2.set_ydata(y2)
+        self.line3.set_ydata(y3)
+        self.fig.canvas.draw()
+
+
+def main():
+    x = np.linspace(0,6*np.pi,1000)
+
+    fs = 25000
+    n = 1000
+
+    scope = TAXScope(fs, n)
+
+    for phase in np.linspace(0,10*np.pi,100):
+        y = np.sin(x+phase)
+        scope.draw(y, y ,y)
+
+
+if __name__=='__main__':
+    main()
